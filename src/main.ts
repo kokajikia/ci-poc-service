54e8af733bae1c1b9cf9b9c2a95ee1d4d5628722
Koka Jikia <akaki.jikia@adjarabet.com>
2026-08-26 15:41:51 +0400

    feat: sample service consuming ci-templates

---
+++ src/main.ts
@@ -0,0 +1,19 @@
+import { NestFactory } from '@nestjs/core';
+import { Module, Controller, Get } from '@nestjs/common';
+
+@Controller()
+class AppController {
+  @Get('/health')
+  health() {
+    return { status: 'ok', service: 'campaign-api' };
+  }
+}
+
+@Module({ controllers: [AppController] })
+class AppModule {}
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+  await app.listen(3000);
+}
+bootstrap();
